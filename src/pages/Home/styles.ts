e5963e85dee85c9fe9173c77ff256b839feded84
danzobiss <danzobiss@gmail.com>
2022-10-26 16:31:09 -0300

    style(Home): adds page centering for wide monitors

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     padding: 48px;
+    max-width: 1500px;
+    margin: auto;
 `;
 
 export const PokemonList = styled.div`
